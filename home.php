a8737185c2cbd4987e3902f1446bf755fc640739
Justin <justin.luczak@hotmail.fr>
2024-01-22 17:38:07 +0100

    responsive slider & title

--- home.php
+++ home.php
@@ -23,9 +23,9 @@ get_header(); ?>
             }
             ?>
 
-            <h1 class="sm:text-6xl flex text-4xl font-extrabold pb-6 uppercase homepage_title">
+            <h1 class="sm:text-6xl flex flex-col sm:flex-row text-4xl font-extrabold pb-6 uppercase homepage_title">
                 <span class="start-title"><?= $end_hero ?></span>
-                <span class="start-title-space">&nbsp;</span>
+                <span class="start-title-space hidden sm:block">&nbsp;</span>
                 <span class="container-words">
                 <?php
                 foreach ($array_hero as $key => $value_word) {
