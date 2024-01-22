e1022f16e5b97f72dc722171bb1e0b2cd2c93bef
Justin <justin.luczak@hotmail.fr>
2024-01-22 15:46:42 +0100

    fix anim with foreach

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,26 +6,32 @@ document.addEventListener("DOMContentLoaded", function(){
 
   let tlWords = gsap.timeline({
     repeat: -1, // Répéter indéfiniment
-    delay: "+=2000ms", // Délai avant de commencer l'animation
+    delay: 2000, // Délai avant de commencer l'animation
   });
 
-  tlWords.from(words, {
-    duration: 0.1, // Durée de l'animation pour chaque mot (100ms)
-    translateY: "100%", // Commence en bas
-    stagger: 0.2, // Délai entre chaque mot
-  });
+  words.forEach((word, index)=>{
 
-  tlWords.to(words, {
-    duration: 0.1,
-    translateY: "0%", // Déplace chaque mot à sa position initiale
-    stagger: 0.2,
-  });
 
-  tlWords.to(words, {
-    delay: 2000,
-    duration: 0.1,
-    translateY: "-100%", // Déplace chaque mot en haut
-    stagger: 0.2,
-  });
+    tlWords.from(word, {
+      duration: 0.1, // Durée de l'animation pour chaque mot (100ms)
+      translateY: "100%", // Commence en bas
+      stagger: 0.2, // Délai entre chaque mot
+    });
+
+    tlWords.to(word, {
+      delay: (index+1) * 2200,
+      duration: 0.1,
+      translateY: "0%", // Déplace chaque mot à sa position initiale
+      stagger: 0.2,
+    });
+
+    tlWords.to(word, {
+      delay: 2000,
+      duration: 0.1,
+      translateY: "-100%", // Déplace chaque mot en haut
+      stagger: 0.2,
+    });
+
+  })
 
 });
--- dist/assets/main-b89e3b89.js
+++ dist/assets/main-b89e3b89.js
@@ -8875,29 +8875,32 @@ document.addEventListener("DOMContentLoaded", function() {
   let tlWords = gsapWithCSS.timeline({
     repeat: -1,
     // Répéter indéfiniment
-    delay: "+=2000ms"
+    delay: 2e3
     // Délai avant de commencer l'animation
   });
-  tlWords.from(words, {
-    duration: 0.1,
-    // Durée de l'animation pour chaque mot (100ms)
-    translateY: "100%",
-    // Commence en bas
-    stagger: 0.2
-    // Délai entre chaque mot
-  });
-  tlWords.to(words, {
-    duration: 0.1,
-    translateY: "0%",
-    // Déplace chaque mot à sa position initiale
-    stagger: 0.2
-  });
-  tlWords.to(words, {
-    delay: 2e3,
-    duration: 0.1,
-    translateY: "-100%",
-    // Déplace chaque mot en haut
-    stagger: 0.2
+  words.forEach((word, index) => {
+    tlWords.from(word, {
+      duration: 0.1,
+      // Durée de l'animation pour chaque mot (100ms)
+      translateY: "100%",
+      // Commence en bas
+      stagger: 0.2
+      // Délai entre chaque mot
+    });
+    tlWords.to(word, {
+      delay: (index + 1) * 2200,
+      duration: 0.1,
+      translateY: "0%",
+      // Déplace chaque mot à sa position initiale
+      stagger: 0.2
+    });
+    tlWords.to(word, {
+      delay: 2e3,
+      duration: 0.1,
+      translateY: "-100%",
+      // Déplace chaque mot en haut
+      stagger: 0.2
+    });
   });
 });
 document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach((title) => {
