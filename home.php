7c0a0074ece1d5258b4615b91261d86592d5ec81
Justin <justin.luczak@hotmail.fr>
2024-01-22 17:40:56 +0100

    fix spacing on h1 mobile

--- home.php
+++ home.php
@@ -23,7 +23,7 @@ get_header(); ?>
             }
             ?>
 
-            <h1 class="sm:text-6xl flex flex-col sm:flex-row text-4xl font-extrabold pb-6 uppercase homepage_title">
+            <h1 class="sm:text-6xl flex flex-col sm:flex-row text-4xl font-extrabold pb-14 sm:pb-6 uppercase homepage_title">
                 <span class="start-title"><?= $end_hero ?></span>
                 <span class="start-title-space hidden sm:block">&nbsp;</span>
                 <span class="container-words">
