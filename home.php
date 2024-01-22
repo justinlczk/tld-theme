1bfa59c3afab5269c71f3b60b7cc9046e74c112a
Justin <justin.luczak@hotmail.fr>
2024-01-22 17:01:31 +0100

    fix for mistake

--- home.php
+++ home.php
@@ -57,7 +57,6 @@ get_header(); ?>
 
             $images_slider_hero = get_field("background_hero");
             $count_images_slider_hero = count($images_slider_hero);
-            var_dump($count_images_slider_hero);
 
 
             foreach ($images_slider_hero as $index_image_slider_hero => $image_slider_hero) {
@@ -71,8 +70,7 @@ get_header(); ?>
 
             <div class="absolute flex flex-col top-1/2 -translate-y-1/2 right-4">
                 <?php
-                for($i = 0; $i++; $i < $count_images_slider_hero) {
-                    var_dump($i);
+                for($i = 0; $i < $count_images_slider_hero; $i++) {
                     ?>
                 <div class="w-4 h-4 rounded-full bg-[#E5E5E5]"></div>
                 <?php
