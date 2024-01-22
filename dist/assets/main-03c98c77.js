08133a5c6fd193f3ace49409db3b22dccadc8295
Justin <justin.luczak@hotmail.fr>
2024-01-22 16:19:33 +0100

    less 4 second of delay

--- dist/assets/main-03c98c77.js
+++ dist/assets/main-03c98c77.js
@@ -8877,7 +8877,7 @@ document.addEventListener("DOMContentLoaded", function() {
       delay: (index + 1) * 2,
       // Décalage pour chaque élément
       repeat: -1,
-      repeatDelay: words.length * 2,
+      repeatDelay: words.length * 2 - 4,
       onComplete: () => {
         if (index < words.length - 1)
           ;
