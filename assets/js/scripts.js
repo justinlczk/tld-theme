859a24735fee10b190ad24ced919bc534057836b
Justin <justin.luczak@hotmail.fr>
2024-01-22 16:09:59 +0100

    add the array length to te delay

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let tl = gsap.timeline({
             delay: index * 2, // Décalage pour chaque élément
             repeat: -1,
-            repeatDelay: index * 2,
+            repeatDelay: (words.length + index) * 2,
             onComplete: () => {
                 if (index < words.length - 1) {
                     //tl.reverse(); // Inverse l'animation après son achèvement
