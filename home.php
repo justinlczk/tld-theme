ad8b5cfdfe701f0540e5862d0e182b64d93fdd96
Justin <justin.luczak@hotmail.fr>
2024-01-26 16:02:21 +0100

    image size

--- home.php
+++ home.php
@@ -63,10 +63,9 @@ get_header();
 
 
             foreach ($images_slider_hero as $index_image_slider_hero => $image_slider_hero) {
-                var_dump($image_slider_hero);
                 ?>
                 <img class="image-slider-hero <?= $index_image_slider_hero === 0 ? "active" : "hidden" ?> object-cover object-center min-h-full min-w-full"
-                     src="<?= $image_slider_hero["sizes"]["large"]["url"] ?>"
+                     src="<?= $image_slider_hero["sizes"]["large"] ?>"
                      alt="<?= $image_slider_hero["alt"] ?>">
                 <?php
             }
